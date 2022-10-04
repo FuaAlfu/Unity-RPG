6f305aeb4f41081a575a3976cd6acf1634da851b
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-10-04 21:25:14 +0300

    add

--- Assets/Scripts/SceneManagement/PortalScene.cs
+++ Assets/Scripts/SceneManagement/PortalScene.cs
@@ -11,11 +11,43 @@ public class PortalScene : MonoBehaviour
     [SerializeField]
     private int nextScene = -1;
 
+    [SerializeField]
+    Transform spawnPoint;
+
     private void OnTriggerEnter(Collider c)
     {
         if(c.tag == "Player")
         {
-            SceneManager.LoadScene(nextScene);
+            //  SceneManager.LoadScene(nextScene);
+            StartCoroutine(Transition());
         }
+    }
+
+    private IEnumerator Transition()
+    {
+        DontDestroyOnLoad(gameObject);
+        yield return SceneManager.LoadSceneAsync(nextScene);
+        PortalScene otherPortal = GetOtherPortal();
+        UpdatePlayer(otherPortal);
+        print("Scene Loaded..");
+        Destroy(gameObject);
+    }
+
+    private void UpdatePlayer(PortalScene otherPortal)
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.transform.position = otherPortal.spawnPoint.position;
+        player.transform.rotation = otherPortal.spawnPoint.rotation;
+    }
+
+    private PortalScene GetOtherPortal()
+    {
+        foreach(PortalScene portal in FindObjectsOfType<PortalScene>())
+        {
+            if (portal == this) continue;
+            return portal;
+        }
+
+        return null;
     }
 }
